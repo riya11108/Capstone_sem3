857cdc1dd3eb2fa611590d9ed000d19203b48d80
DereK <derek@Sarabjeets-MacBook-Pro.local>
2025-12-09 03:07:03 +0530

    bugs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -109,7 +109,7 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None', secure: true }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
@@ -124,7 +124,7 @@ export const login = async (req, res) => {
 }
 export const logout = async (req, res) => {
     try {
-        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+        return res.status(200).cookie("token", "", { maxAge: 0, httpOnly: true, sameSite: 'None', secure: true }).json({
             message: "Logged out successfully.",
             success: true
         })
